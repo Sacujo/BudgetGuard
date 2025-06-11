b358516e7eba24794b6fe90618fd873df6194aee
Igor Guryan <sacujo93@yandex.ru>
2025-06-12 01:48:39 +0300

    Initial Commit

---
+++ BudgetGuard/BudgetGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetGuardApp.swift
+//  BudgetGuard
+//
+//  Created by Igor Guryan on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct BudgetGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
